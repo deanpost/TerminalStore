bddac5c67b3318789ec41c6bbc7647e431480db9
W1-F1 <133356806+deanpost@users.noreply.github.com>
2023-10-25 17:24:36 +0200

    Update of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 user=$(logname)
 cd ~
-sudo cp /home/$user/TerminalStore/terminalstore /usr/local/bin/
-cd /usr/local/bin/
+sudo cp /home/$user/TerminalStore/terminalstore /usr/bin/
+cd /usr/bin/
 sudo chmod +x terminalstore
 sudo chmod +x /home/$user/TerminalStore/terminalstore.py
