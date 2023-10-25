6561d902d3fb4deeaef980a4e91796d4f1a38a41
W1-F1 <133356806+deanpost@users.noreply.github.com>
2023-10-25 17:20:08 +0200

    Update of setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,5 @@ print("Installing TerminalStore...")
 #For bugtesting
 #print("Your /usr/bin path(s)")
 #os.system('echo ${PATH}')
-os.system('cd ~')
-os.system('sudo cp terminalstore /usr/local/bin/')
-os.system('cd /usr/local/bin/')
-os.system('sudo chmod +x terminalstore')
-os.system('sudo chmod +x ~/TerminalStore/terminalstore.py')
+os.system("sudo bash setup.sh")
 print("Done!")
-     
